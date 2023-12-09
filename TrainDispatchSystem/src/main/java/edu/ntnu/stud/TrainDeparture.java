066923bfe01bb60c1739be3d13fcf7a08bd4fdcd
Snake727 <markusnikolai727@gmail.com>
2023-12-09 01:33:22 +0100

    Updated methods and documentation

--- TrainDispatchSystem/src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ TrainDispatchSystem/src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -10,7 +10,7 @@ import java.time.LocalTime;
  * LocalTime, String, int, Duration
  * These data types fit the variables because:
  * departureTime is a time, line is a string, trainNumber is an integer, destination is a string, track is an integer, delay is a duration.
- * @version 1.2 2023-12-07
+ * @version 1.3 2023-12-08
  * */
 
 public class TrainDeparture {
@@ -42,11 +42,11 @@ public class TrainDeparture {
     /**
      * Sets the departure time.
      * @param departureTime the departure time
-     * @throws IllegalArgumentException if the departure time is null, above 23:59 or below 00:00
+     * @throws IllegalArgumentException if the departure time is null. The class LocalTime has built-in checks for time being between 00:00 and 23:59.
      * */
     public void setDepartureTime(LocalTime departureTime) {
-        if (departureTime == null || departureTime.isAfter(LocalTime.of(23, 59) ) || departureTime.isBefore(LocalTime.of(0, 0))) {
-            throw new IllegalArgumentException("Departure time must be between 00:00 and 23:59");
+        if (departureTime== null) {
+            throw new IllegalArgumentException("Departure time cannot be null");
         } else {
             this.departureTime = departureTime;
         }
@@ -142,41 +142,56 @@ public class TrainDeparture {
     }
 
     /**
-     * Sets the delay.
-     * @param delay the delay
+     * Sets the delay. Requires both hours and minutes to work.
+     * @param hours the amount of hours to delay the train departure.
+     * @param minutes the amount of minutes to delay the train departure.
      * @throws IllegalArgumentException if the delay is null, or if the delay is negative.
-     * */
+     */
 
-    public void setDelay(Duration delay) {
-        if (delay == null || delay.isNegative() || delay.isZero()) {
-            throw new IllegalArgumentException("Delay must be a positive number");
+    public void setDelay(Duration hours, Duration minutes) {
+        if (hours == null || minutes == null) {
+            throw new IllegalArgumentException("Delay cannot be null");
+        } else if (hours.isNegative() || minutes.isNegative()) {
+            throw new IllegalArgumentException("Delay cannot be negative");
         } else {
-            this.delay = delay;
+            this.delay = hours.plus(minutes);
         }
     }
 
     /**
-     * Checks if the train has departed.
+     * Checks if the train has departed by comparing the current time to the departure time and delay.
      * @return true if the train has departed, false if not.
      * */
 
     public boolean hasDeparted(LocalTime currentTime) {
-        return currentTime.isAfter(departureTime);
+        return currentTime.isAfter(departureTime.plus(delay));
     }
 
     /**
-     * Prints the details of the train departure.
+     * Prints the details of the train departure. If the train has no delay, the delay is set to "No delay". Otherwise, the delay is printed in hours and minutes.
      * @return the details of a given train departure
      * */
 
     public String printDetails() {
-        return "TrainDeparture{" +
-                "departureTime=" + departureTime +
-                ", line='" + line + '\'' +
-                ", trainNumber=" + trainNumber +
-                ", destination='" + destination + '\'' +
-                ", track=" + track +
-                ", delay=" + delay +
-                '}';
+        long hours = delay.toHoursPart();
+        long minutes = delay.toMinutesPart();
+        String delayString = "";
+
+        // Checks if there is a delay. If there is a delay, checks if the delay is in hours, minutes or both.
+        if (hours > 0) {
+            delayString = String.format("%d hours %d minutes", hours, minutes);
+        } else if (minutes > 0) {
+            delayString = String.format("%d minutes", minutes);
+        } else if (minutes == 0) {
+            delayString = "No delay";
+        }
+
+        return "Train departure details: " +
+                "departure-time = " + departureTime +
+                ", line = " + line +
+                ", train-number = " + trainNumber +
+                ", destination = " + destination +
+                ", track = " + track +
+                ", delay = " + delayString;
     }
 }
