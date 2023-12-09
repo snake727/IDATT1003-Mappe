33adabb57bd47343ba42e4895105627363921246
Snake727 <markusnikolai727@gmail.com>
2023-12-09 05:27:19 +0100

    Added UI into Main

--- TrainDispatchSystem/src/main/java/edu/ntnu/stud/Main.java
+++ TrainDispatchSystem/src/main/java/edu/ntnu/stud/Main.java
@@ -1,16 +1,16 @@
 package edu.ntnu.stud;
 
-import java.util.Scanner;
-
 /**
  * This class represents the main part of the program.
  *
- * @version 1.0 2023-12-07
+ * @version 1.1 2023-12-08
  *
  * */
 
 public class Main {
     public static void main (String[] args){
-
+        UserInterface ui = new UserInterface();
+        ui.start();
+        ui.init();
     }
 }
