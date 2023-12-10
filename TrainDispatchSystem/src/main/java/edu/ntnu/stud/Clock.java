825003328b24c22a3bbfeba3a3b3ce24900e2d70
Snake727 <markusnikolai727@gmail.com>
2023-12-10 14:34:42 +0100

    Last polish on Clock class documentation

--- TrainDispatchSystem/src/main/java/edu/ntnu/stud/Clock.java
+++ TrainDispatchSystem/src/main/java/edu/ntnu/stud/Clock.java
@@ -5,7 +5,7 @@ import java.time.LocalTime;
 /**
  * This class represents a clock, and its sole purpose is to keep track of time, and provide methods for dealing with time.
  *
- * @version 1.1 2023-12-08
+ * @version 1.2 2023-12-10
  *
  * */
 
@@ -20,7 +20,7 @@ public class Clock {
     }
 
     /**
-     * Gets the time.
+     * Simple method that returns the current time of the clock.
      * @return the time
      * */
     public LocalTime getCurrentTime() {
@@ -28,7 +28,7 @@ public class Clock {
     }
 
     /**
-     * Sets the time. Time cannot be before current time.
+     * Method that sets the time. Time cannot be before current time. Added logic to make sure time is between 00:00 and 23:59, and that it is not before current time, as this is a requirement for its task.
      * @param time the time
      * @throws IllegalArgumentException if the time is null, above 23:59 or below 00:00
      * @throws IllegalStateException if the time is before current time
@@ -44,7 +44,7 @@ public class Clock {
     }
 
     /**
-     * Resets the time back to 00:00.
+     * Simple method that resets the time back to 00:00. This is needed for resetting the clock in the UI, as the clock cannot be set back by any other means.
      * */
     public void resetTime() {
         time = LocalTime.of(0, 0);
