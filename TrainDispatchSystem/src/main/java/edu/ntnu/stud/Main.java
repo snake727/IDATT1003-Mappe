936da3732c310f9e734b04319ac259bade7896b8
Snake727 <markusnikolai727@gmail.com>
2023-12-11 02:53:10 +0100

    Checkstyle ok. Fixed the order of init and start

--- TrainDispatchSystem/src/main/java/edu/ntnu/stud/Main.java
+++ TrainDispatchSystem/src/main/java/edu/ntnu/stud/Main.java
@@ -2,15 +2,20 @@ package edu.ntnu.stud;
 
 /**
  * This class represents the main part of the program.
+ * This is kept as simple as possible, and only contains the main method.
+ * Inside the main method, the UserInterface class is instantiated.
+ * And the start and init methods are called.
  *
- * @version 1.1 2023-12-08
- *
+ * @version 1.2 2023-12-11
  * */
 
 public class Main {
-    public static void main (String[] args){
-        UserInterface ui = new UserInterface();
-        ui.start();
-        ui.init();
-    }
+  /**
+   * Starts the program.
+   * */
+  public static void main(String[] args) {
+    UserInterface ui = new UserInterface();
+    ui.init();
+    ui.start();
+  }
 }
